99474adc8f54dada7c5e3abebafc5d1a4c8094c4
square <admin@squarecode.de>
2024-04-18 17:53:22 +0200

    Refactor invalid move detection for better readability and structure
    The InvalidMoveDetection class was significantly refactored to improve readability, maintainability, and overall code structure. Comments were added to explain the purpose and functions of the variables and methods within the class. Moreover, the logic for invalid move detection was broken down into smaller, modular methods aiming to enhance the code clarity and simplify the debugging process. The refactoring makes it easier to understand the login for preventing invalid move, checking for forbidden borders, handling extreme values, processing buffer and travel time, and sorting out invalid rotation and traveled chunks.

--- src/main/java/de/feelix/sierra/check/impl/move/InvalidMoveDetection.java
+++ src/main/java/de/feelix/sierra/check/impl/move/InvalidMoveDetection.java
@@ -21,99 +21,264 @@ import de.feelix.sierraapi.violation.PunishType;
 @SierraCheckData(checkType = CheckType.MOVE)
 public class InvalidMoveDetection extends SierraDetection implements IngoingProcessor {
 
-
+    /**
+     * Represents the hard-coded border value used for invalid move detection.
+     *
+     * <p>
+     * The HARD_CODED_BORDER variable is a private constant in the InvalidMoveDetection class.
+     * It is of type double and has a value of 2.9999999E7D.
+     * </p>
+     *
+     * <p>
+     * The hard-coded border value is used to check if a player's movement exceeds the valid boundary.
+     * If the player's position is greater than the hard-coded border value, it is considered an invalid move.
+     * </p>
+     *
+     * <p>
+     * This variable is used in various methods within the InvalidMoveDetection class to handle and detect invalid moves.
+     * </p>
+     */
     private static final double HARD_CODED_BORDER = 2.9999999E7D;
 
+    /**
+     * Represents the last chunk ID.
+     *
+     * <p>
+     * The lastChunkId variable stores the ID of the last chunk that was processed.
+     * It is used in the InvalidMoveDetection class to track the chunks that the player has moved through.
+     * </p>
+     *
+     * @since 1.0
+     */
     private double lastChunkId = -1;
+
+    /**
+     * Represents the timestamp of the last tick.
+     *
+     * <p>
+     * The lastTick variable stores the timestamp of the last tick, which is a unit of time measurement used in the game.
+     * The value is initialized to -1, indicating that no tick has occurred yet.
+     * </p>
+     *
+     * @since 1.0
+     * @see InvalidMoveDetection
+     */
     private long   lastTick    = -1;
+
+    /**
+     * Represents a private integer variable named buffer.
+     * <p>
+     * The buffer variable stores an integer value that is used within the class {@link InvalidMoveDetection}.
+     * The purpose of this variable is not specified in the given code snippet.
+     * </p>
+     * <p>
+     * This variable is declared with private access specifier, meaning it can only be accessed within the same class.
+     * </p>
+     * <p>
+     * The initial value of the buffer variable is set to 0.
+     * </p>
+     * <p>
+     * It is important to note that the buffer variable is not returned or used in any example code provided.
+     * </p>
+     *
+     * @see InvalidMoveDetection
+     * @since 1.0
+     */
     private int    buffer      = 0;
 
+    /**
+     * InvalidMoveDetection is a subclass of SierraDetection which is used to detect and handle invalid movement packets from a player.
+     * It examines the player's movement data and checks for any suspicious or invalid values.
+     *
+     * @param playerData The PlayerData object containing the player's data
+     */
     public InvalidMoveDetection(PlayerData playerData) {
         super(playerData);
     }
 
+    /**
+     * This method handles the PacketReceiveEvent by checking for invalid movement and taking appropriate actions.
+     *
+     * @param event the PacketReceiveEvent triggered by receiving a packet from the player
+     * @param data  the PlayerData object containing the player's data
+     */
     @Override
     public void handle(PacketReceiveEvent event, PlayerData data) {
 
-        if (!Sierra.getPlugin().getSierraConfigEngine().config().getBoolean("prevent-invalid-move", true)) {
+        if (isPreventInvalidMoveDisabled()) {
             return;
         }
-
-        if (WrapperPlayClientPlayerFlying.isFlying(event.getPacketType())) {
-
-            WrapperPlayClientPlayerFlying wrapper = new WrapperPlayClientPlayerFlying(event);
-
-            Location location = wrapper.getLocation();
-
-            if (wrapper.hasRotationChanged()) checkInvalidPitch(location, event);
-
-            if (!wrapper.hasPositionChanged()) return;
-
-            Vector3d position = location.getPosition();
-
-            double chunkId = Math.floor(position.getX() / 32) + Math.floor(position.getZ() / 32);
-
-            long tick = System.currentTimeMillis();
-
-            if (chunkId != lastChunkId) {
-
-                long travelTime = tick - this.lastTick;
-
-                if (travelTime < 20) {
-                    buffer++;
-
-                    if (buffer > 5) {
-                        this.violation(event, ViolationDocument.builder()
-                            .punishType(PunishType.KICK)
-                            .debugInformation(String.format("Traveled %d chunks in ~%dms", buffer, travelTime))
-                            .build());
-                    }
-                } else {
-                    buffer = Math.max(0, buffer - 1);
-                }
-                this.lastTick = tick;
-            }
-            this.lastChunkId = chunkId;
-
-            checkForBorder(position, event);
-
-            if (invalidValue(location.getX(), location.getY(), location.getZ())) {
-                violation(
-                    event, ViolationDocument.builder()
-                        .debugInformation("Extreme values: double")
-                        .punishType(PunishType.BAN)
-                        .build());
-            }
-
-            if (invalidValue(location.getYaw(), location.getPitch())) {
-                violation(
-                    event, ViolationDocument.builder()
-                        .debugInformation("Extreme values: float")
-                        .punishType(PunishType.BAN)
-                        .build());
-            }
-
-        } else if (event.getPacketType() == PacketType.Play.Client.VEHICLE_MOVE) {
-            WrapperPlayClientVehicleMove wrapper = new WrapperPlayClientVehicleMove(event);
-
-            Vector3d location = wrapper.getPosition();
-
-            if (invalidValue(location.getX(), location.getY(), location.getZ())) {
-                violation(event, ViolationDocument.builder()
-                    .debugInformation("Extreme values: double")
-                    .punishType(PunishType.BAN)
-                    .build());
-            }
-
-            if (invalidValue(wrapper.getYaw(), wrapper.getPitch())) {
-                violation(event, ViolationDocument.builder()
-                    .debugInformation("Extreme values: float")
-                    .punishType(PunishType.BAN)
-                    .build());
-            }
+        if (isFlying(event)) {
+            handleFlyingPacket(event);
+        } else if (isVehicleMovePacket(event)) {
+            handleVehicleMovePacket(event);
         }
     }
 
+    /**
+     * Determines if the prevent-invalid-move feature is disabled.
+     *
+     * @return true if the prevent-invalid-move feature is disabled, false otherwise.
+     */
+    private boolean isPreventInvalidMoveDisabled() {
+        return !Sierra.getPlugin().getSierraConfigEngine().config().getBoolean("prevent-invalid-move", true);
+    }
+
+    /**
+     * Determines if the given PacketReceiveEvent represents a flying packet.
+     *
+     * @param event the PacketReceiveEvent triggered by receiving a packet from the player
+     * @return true if the packet type is flying, false otherwise
+     */
+    private boolean isFlying(PacketReceiveEvent event) {
+        return WrapperPlayClientPlayerFlying.isFlying(event.getPacketType());
+    }
+
+    /**
+     * Determines if the given {@link PacketReceiveEvent} represents a vehicle move packet.
+     *
+     * @param event the {@link PacketReceiveEvent} triggered by receiving a packet from the player
+     * @return true if the packet type is {@code PacketType.Play.Client.VEHICLE_MOVE}, false otherwise
+     */
+    private boolean isVehicleMovePacket(PacketReceiveEvent event) {
+        return event.getPacketType() == PacketType.Play.Client.VEHICLE_MOVE;
+    }
+
+    /**
+     * This method handles the flying packet received from the player.
+     *
+     * @param event the PacketReceiveEvent triggered by receiving a packet from the player
+     */
+    private void handleFlyingPacket(PacketReceiveEvent event) {
+        WrapperPlayClientPlayerFlying wrapper = new WrapperPlayClientPlayerFlying(event);
+
+        sortOutInvalidRotation(wrapper, event);
+        if (!wrapper.hasPositionChanged()) {
+            return;
+        }
+
+        Location location = wrapper.getLocation();
+        Vector3d position = location.getPosition();
+        double   chunkId  = computeChunkId(position);
+
+        sortOutTraveledChunks(chunkId, event);
+        lastChunkId = chunkId;
+
+        checkForBorder(position, event);
+        sortOutExtremeValues(location, event);
+    }
+
+    /**
+     * Sorts out invalid rotation data from a flying packet.
+     *
+     * @param wrapper the WrapperPlayClientPlayerFlying object containing the flying packet data
+     * @param event   the PacketReceiveEvent triggered by receiving the packet from the player
+     */
+    private void sortOutInvalidRotation(WrapperPlayClientPlayerFlying wrapper, PacketReceiveEvent event) {
+        if (wrapper.hasRotationChanged()) checkInvalidPitch(wrapper.getLocation(), event);
+    }
+
+    /**
+     * Sorts out traveled chunks based on the chunkId and the current system time.
+     *
+     * @param chunkId the ID of the chunk traveled to
+     * @param event   the PacketReceiveEvent triggered by receiving a packet
+     */
+    private void sortOutTraveledChunks(double chunkId, PacketReceiveEvent event) {
+        long tick = System.currentTimeMillis();
+        if (chunkId == lastChunkId) {
+            return;
+        }
+
+        long travelTime = tick - this.lastTick;
+        processBufferAndViolation(travelTime, event);
+
+        this.lastTick = tick;
+    }
+
+    /**
+     * Process the buffer and handle violation if necessary.
+     *
+     * @param travelTime the travel time in milliseconds
+     * @param event      the PacketReceiveEvent triggered by receiving a packet from the player
+     */
+    private void processBufferAndViolation(long travelTime, PacketReceiveEvent event) {
+        if (travelTime < 20) {
+            buffer++;
+            if (buffer > 5) {
+                this.violation(event, ViolationDocument.builder()
+                    .punishType(PunishType.KICK)
+                    .debugInformation(String.format("Traveled %d chunks in ~%dms", buffer, (travelTime / buffer)))
+                    .build());
+            }
+        } else {
+            buffer = Math.max(0, buffer - 1);
+        }
+    }
+
+    /**
+     * Sorts out extreme values from the given location.
+     *
+     * @param location the location to check for extreme values
+     * @param event the PacketReceiveEvent triggered by receiving a packet from the player
+     */
+    private void sortOutExtremeValues(Location location, PacketReceiveEvent event) {
+        if (invalidValue(location.getX(), location.getY(), location.getZ())) {
+            violation(event, buildExtValueBanViolation("Extreme values: double"));
+        }
+
+        if (invalidValue(location.getYaw(), location.getPitch())) {
+            violation(event, buildExtValueBanViolation("Extreme values: float"));
+        }
+    }
+
+    /**
+     * Builds a violation document with an extended value ban punishment type.
+     *
+     * @param debugInfo the debug information related to the violation document
+     * @return the built violation document
+     */
+    private ViolationDocument buildExtValueBanViolation(String debugInfo) {
+        return ViolationDocument.builder()
+            .debugInformation(debugInfo)
+            .punishType(PunishType.BAN)
+            .build();
+    }
+
+    /**
+     * Handles a vehicle move packet received from the player.
+     *
+     * @param event The PacketReceiveEvent triggered by receiving a packet from the player.
+     */
+    private void handleVehicleMovePacket(PacketReceiveEvent event) {
+        WrapperPlayClientVehicleMove wrapper  = new WrapperPlayClientVehicleMove(event);
+        Vector3d                     location = wrapper.getPosition();
+
+        if (invalidValue(location.getX(), location.getY(), location.getZ())) {
+            violation(event, buildExtValueBanViolation("Extreme values: double"));
+        }
+
+        if (invalidValue(wrapper.getYaw(), wrapper.getPitch())) {
+            violation(event, buildExtValueBanViolation("Extreme values: float"));
+        }
+    }
+
+    /**
+     * Computes the chunk ID based on the given position.
+     *
+     * @param position the position of the entity
+     * @return the computed chunk ID
+     */
+    private double computeChunkId(Vector3d position) {
+        return Math.floor(position.getX() / 32) + Math.floor(position.getZ() / 32);
+    }
+
+    /**
+     * Checks if any of the given float values is Infinite.
+     *
+     * @param value The float values to check for Infinity.
+     * @return {@code true} if any of the values is Infinite, {@code false} otherwise.
+     */
     public boolean invalidValue(float... value) {
         for (float v : value) {
             if (Float.isInfinite(v)) {
@@ -123,6 +288,12 @@ public class InvalidMoveDetection extends SierraDetection implements IngoingProc
         return false;
     }
 
+    /**
+     * Determines if any of the given double values is NaN or infinite.
+     *
+     * @param value The double values to check for NaN or infinite.
+     * @return {@code true} if any of the values is NaN or infinite, {@code false} otherwise.
+     */
     public boolean invalidValue(double... value) {
         for (double v : value) {
             if (Double.isNaN(v) || Double.isInfinite(v)) {
@@ -132,6 +303,12 @@ public class InvalidMoveDetection extends SierraDetection implements IngoingProc
         return false;
     }
 
+    /**
+     * Check if the position is outside the border and handle violation accordingly.
+     *
+     * @param position The position to check
+     * @param event    The event triggered by receiving a packet from the player
+     */
     private void checkForBorder(Vector3d position, PacketReceiveEvent event) {
         if (Math.abs(position.getX()) > HARD_CODED_BORDER || Math.abs(position.getY()) > HARD_CODED_BORDER
             || Math.abs(position.getZ()) > HARD_CODED_BORDER) {
@@ -143,6 +320,12 @@ public class InvalidMoveDetection extends SierraDetection implements IngoingProc
         }
     }
 
+    /**
+     * Checks for invalid pitch in the given location and handles violation if necessary.
+     *
+     * @param location The location to check for invalid pitch
+     * @param event The PacketReceiveEvent triggered by receiving a packet from the player
+     */
     private void checkInvalidPitch(Location location, PacketReceiveEvent event) {
         float pitch = location.getPitch();
         if (Math.abs(pitch) > 90.01) {
