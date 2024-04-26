e17ba121e2124811b1377de969a649609bf8c83a
square <admin@squarecode.de>
2024-04-26 21:40:56 +0200

    Add detailed JavaDoc comments to SierraAPI's module interfaces
    Added comprehensive JavaDoc comments to the SierraAPI's module interface files (Module, ModuleGateway, ModuleDescription). These comments provide detailed explanations for what these interfaces are and what their methods do, significantly improving the code's readability and maintainability.

--- api/src/main/java/de/feelix/sierraapi/module/ModuleDescription.java
+++ api/src/main/java/de/feelix/sierraapi/module/ModuleDescription.java
@@ -1,12 +1,36 @@
 package de.feelix.sierraapi.module;
 
+/**
+ * The ModuleDescription interface represents the description of a module.
+ * It provides information about the module's name, main class, author, and version.
+ */
 public interface ModuleDescription {
 
+    /**
+     * Returns the name of the module.
+     *
+     * @return the name of the module
+     */
     String name();
 
+    /**
+     * Returns the value of the main method of the module.
+     *
+     * @return the value of the main method
+     */
     String main();
 
+    /**
+     * Returns the author of the module.
+     *
+     * @return the author of the module
+     */
     String author();
 
+    /**
+     * Returns the version of the module.
+     *
+     * @return the version of the module
+     */
     String version();
 }
--- api/src/main/java/de/feelix/sierraapi/module/ModuleGateway.java
+++ api/src/main/java/de/feelix/sierraapi/module/ModuleGateway.java
@@ -3,15 +3,46 @@ package de.feelix.sierraapi.module;
 import java.io.File;
 import java.util.Map;
 
+/**
+ * ModuleGateway is an interface for managing modules in the Sierra plugin.
+ */
 public interface ModuleGateway {
 
+    /**
+     * Returns the directory where the module is located.
+     *
+     * @return The directory where the module is located.
+     */
     File moduleDirectory();
 
+    /**
+     * Retrieves a map of module names to module objects.
+     *
+     * @return A map of module names to module objects.
+     */
     Map<String, Module> modules();
 
+    /**
+     * Checks if a module with the given name is activated.
+     *
+     * @param moduleName The name of the module to check.
+     * @return true if the module is activated, false otherwise.
+     */
     boolean moduleActivated(String moduleName);
 
+    /**
+     * Deactivates a module with the given moduleName.
+     *
+     * @param moduleName The name of the module to deactivate.
+     * @return true if the module was successfully deactivated, false otherwise.
+     */
     boolean deactivateModule(String moduleName);
 
+    /**
+     * Activates a module with the given moduleName.
+     *
+     * @param moduleName The name of the module to activate.
+     * @return true if the module was successfully activated, false otherwise.
+     */
     boolean activateModule(String moduleName);
 }
