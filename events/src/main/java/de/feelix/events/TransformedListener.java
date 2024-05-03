afce62dd5eb4cd6e19e4567c80551c07a02462e7
square <admin@squarecode.de>
2024-05-03 22:39:52 +0200

    Add documentation to TransformedListener class
    The commit adds Javadoc comments to the TransformedListener class in the events module, clarifying the purpose and usage of the class and its fields. This will enhance code readability and maintainability, assisting developers understand the responsibility each attribute holds within the class.

--- events/src/main/java/de/feelix/events/TransformedListener.java
+++ events/src/main/java/de/feelix/events/TransformedListener.java
@@ -6,11 +6,28 @@ import lombok.Data;
 
 import java.lang.reflect.Method;
 
+/**
+ * Represents a listener that has been transformed and registered in the EventManager.
+ */
 @Data
 @AllArgsConstructor
 public class TransformedListener {
 
+    /**
+     * Represents an instance of a listener class.
+     */
     private final Object           listenerClassInstance;
+
+    /**
+     * Represents a method that is invoked by a listener.
+     */
     private final Method           listenerMethod;
+
+    /**
+     * Represents the priority of an event listener.
+     *
+     * <p>
+     * The priority determines the order in which the listeners are invoked when an event occurs.
+     * The priorities are defined in the {@link ListenerPriority} enum with the following values*/
     private final ListenerPriority priority;
 }
