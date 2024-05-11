225bbe6468ebb17b7f01e5fca96dc3b1b9eef3ed
square <admin@squarecode.de>
2024-05-11 15:07:17 +0200

    Add new attributes and reorganize AttributeMapper
    The AttributeMapper enum in AttributeMapper.java was updated and reorganized. Several new attributes were added, such as PlayerBlockInteraction, PlayerEntityRange, PlayerBlockBreak, Gravity, FallDistance, FallDamageMultiplier, and many others. The order of some existing attributes was also changed for better readability and structural efficiency.

--- src/main/java/de/feelix/sierra/utilities/AttributeMapper.java
+++ src/main/java/de/feelix/sierra/utilities/AttributeMapper.java
@@ -11,17 +11,49 @@ import lombok.Getter;
 public enum AttributeMapper {
 
     Armor(new String[]{"generic.armor"}, 30, 0),
+
     ArmorTough(new String[]{"generic.armorToughness", "generic.armor_toughness"}, 20, 0),
+
     AttackDamage(new String[]{"generic.attackDamage", "generic.attack_damage"}, 2.048, 0),
-    AttackSpeed(new String[]{"generic.attackSpeed", "generic.attack_speed"}, 1.024, 0),
+
     AttackKnockback(new String[]{"generic.attack_knockback", "generic.attack_knockback"}, 5, 0),
+
+    AttackSpeed(new String[]{"generic.attackSpeed", "generic.attack_speed"}, 1.024, 0),
+
     FlyingSpeed(new String[]{"generic.flyingSpeed", "generic.flying_speed"}, 1.024, 0),
-    JumpStrength(new String[]{"horse.jumpStrength", "horse.jump_strength"}, 2, 0),
+
+    HoseJumpStrength(new String[]{"horse.jumpStrength", "horse.jump_strength"}, 2, 0),
+
+    JumpStrength(new String[]{"generic.jump_strength", "generic.jumpStrength"}, 32, 0),
+
+    PlayerBlockInteraction(new String[]{"player.block_interaction_range", "player.blockInteractionRange"}, 64, 0),
+
+    PlayerEntityRange(new String[]{"player.entity_interaction_range", "player.entityInteractionRange"}, 64, 0),
+
+    PlayerBlockBreak(new String[]{"player.block_break_speed", "player.blockBreakSpeed"}, 1024, 0),
+
     KnockbackResistance(new String[]{"generic.knockbackResistance", "generic.knockback_resistance"}, 1, 0),
+
     Luck(new String[]{"generic.luck"}, 1.024, -1.024),
-    MaxHealth(new String[]{"generic.maxHealth", "generic.max_health"}, 1.024, 0),
+
+    Gravity(new String[]{"generic.gravity"}, 1, -1),
+
+    FallDistance(new String[]{"generic.safe_fall_distance", "generic.safeFallDistance"}, 1.024, -1.024),
+
+    FallDamageMultiplier(new String[]{"generic.fall_damage_multiplier", "generic.fallDamageMultiplier"}, 100, 0),
+
+    MaxHealth(new String[]{"generic.maxHealth", "generic.max_health"}, 1, -1),
+
+    MaxAbsorption(new String[]{"generic.max_absorption", "generic.maxAbsorption"}, 2.048, 0),
+
+    Scale(new String[]{"generic.scale"}, 16, 0.0625),
+
+    StepHeight(new String[]{"generic.step_height", "generic.stepHeight"}, 10, 0),
+
     FollowRange(new String[]{"generic.followRange", "generic.follow_range"}, 2.048, 0),
+
     MovementSpeed(new String[]{"generic.movementSpeed", "generic.movement_speed"}, 1.024, 0),
+
     SpawnReinforcements(new String[]{"zombie.spawnReinforcements", "zombie.spawn_reinforcements"}, 1, 0);
 
     /**
