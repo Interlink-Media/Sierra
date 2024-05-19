4c8db7e16e96019f1fce6121b5b10fb9e08e572d
square <admin@squarecode.de>
2024-05-19 12:56:51 +0200

    Updated monitor message formatting
    The commit includes updates to the MonitorCommand's message formatting. Instead of hard-coding the messages, they are now dynamically retrieved from the messages.yml file. This also includes formatting the timings to be detailed in ms.

--- src/main/java/de/feelix/sierra/command/impl/MonitorCommand.java
+++ src/main/java/de/feelix/sierra/command/impl/MonitorCommand.java
@@ -3,6 +3,7 @@ package de.feelix.sierra.command.impl;
 import com.github.retrooper.packetevents.protocol.player.User;
 import de.feelix.sierra.Sierra;
 import de.feelix.sierra.manager.storage.PlayerData;
+import de.feelix.sierra.utilities.message.ConfigValue;
 import de.feelix.sierraapi.commands.*;
 import de.feelix.sierraapi.timing.Timing;
 import de.feelix.sierraapi.timing.TimingHandler;
@@ -20,43 +21,63 @@ import java.util.List;
 public class MonitorCommand implements ISierraCommand {
 
     /**
-     * The process method is responsible for processing the command and printing the performance monitor information related to the player.
+     * The process method is responsible for processing the command and printing the performance monitor information
+     * related to the player.
      *
-     * @param user              the User associated with the command execution
-     * @param sierraUser        the SierraUser representing the user in the Sierra API
-     * @param abstractCommand   the IBukkitAbstractCommand object representing the wrapped Bukkit Command
-     * @param sierraLabel       the ISierraLabel object representing the label of the initial symbol
-     * @param sierraArguments   the ISierraArguments object representing the arguments passed with the command
+     * @param user            the User associated with the command execution
+     * @param sierraUser      the SierraUser representing the user in the Sierra API
+     * @param abstractCommand the IBukkitAbstractCommand object representing the wrapped Bukkit Command
+     * @param sierraLabel     the ISierraLabel object representing the label of the initial symbol
+     * @param sierraArguments the ISierraArguments object representing the arguments passed with the command
      */
     @Override
-    public void process(User user, SierraUser sierraUser, IBukkitAbstractCommand abstractCommand, ISierraLabel sierraLabel,
+    public void process(User user, SierraUser sierraUser, IBukkitAbstractCommand abstractCommand,
+                        ISierraLabel sierraLabel,
                         ISierraArguments sierraArguments) {
 
-        user.sendMessage(Sierra.PREFIX + " §fPerformance monitor §7(Your data)");
+        user.sendMessage(new ConfigValue(
+            "commands.monitor.header",
+            "{prefix} &fPerformance monitor &7(Your data)",
+            true
+        ).replacePrefix().colorize().getMessageValue());
 
         WeakReference<PlayerData> playerData = Sierra.getPlugin().getSierraDataManager().getPlayerData(user);
 
         if (playerData == null || playerData.get() == null) {
-            user.sendMessage(Sierra.PREFIX + " §cNo data found");
+            user.sendMessage(
+                new ConfigValue(
+                    "commands.monitor.nothing-found",
+                    "{prefix} &cNo data found!",
+                    true
+                ).replacePrefix().colorize().getMessageValue());
             return;
         }
-
         printMonitor(user, sierraUser);
     }
 
     /**
      * Prints performance monitor information related to the player.
      *
-     * @param user the User associated with the command execution
+     * @param user       the User associated with the command execution
      * @param sierraUser the SierraUser representing the user in the Sierra API
      */
     private void printMonitor(User user, SierraUser sierraUser) {
 
         TimingHandler timingProcessor = sierraUser.timingHandler();
-        user.sendMessage(Sierra.PREFIX + " §c§lPackets:");
+        user.sendMessage(
+            new ConfigValue(
+                "commands.monitor.packets-header",
+                "{prefix} &c&lPackets:",
+                true
+            ).replacePrefix().colorize().getMessageValue());
         sendTiming(timingProcessor.getPacketReceiveTask(), "Ingoing Packets", user);
         sendTiming(timingProcessor.getPacketSendTask(), "Outgoing Packets", user);
-        user.sendMessage(Sierra.PREFIX + " §c§lEnvironment:");
+        user.sendMessage(
+            new ConfigValue(
+                "commands.monitor.environment-header",
+                "{prefix} &c&lEnvironment:",
+                true
+            ).replacePrefix().colorize().getMessageValue());
         sendTiming(timingProcessor.getMovementTask(), "Movement Task", user);
     }
 
@@ -64,11 +85,19 @@ public class MonitorCommand implements ISierraCommand {
      * Sends a timing message to the user.
      *
      * @param packetReceiveTiming the Timing object representing the timing of the received packet
-     * @param title the title of the timing message
-     * @param user the User object to send the message to
+     * @param title               the title of the timing message
+     * @param user                the User object to send the message to
      */
     private void sendTiming(Timing packetReceiveTiming, String title, User user) {
-        user.sendMessage(String.format("%s  §8- §f%s §7(%.5fms)", Sierra.PREFIX, title, packetReceiveTiming.delay()));
+        user.sendMessage(
+            new ConfigValue(
+                "commands.monitor.entry",
+                "{prefix}  &8- &f{title} &7({delay}ms)",
+                true
+            ).replacePrefix()
+                .replace("{title}", title)
+                .replace("{delay}", String.format("%.5f", packetReceiveTiming.delay()))
+                .colorize().getMessageValue());
     }
 
     /**
