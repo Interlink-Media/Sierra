b5498442c7cd89c600bc198e40fd2c3c36eef77b
square <admin@squarecode.de>
2024-05-24 23:07:34 +0200

    Refactor code to reset ban and kick metrics after reporting
    The logic has been modified for reporting the 'bans' and 'kicks' metrics. Now instead of just returning the metrics, the corresponding count in the SierraDataManager is reset to 0 after the metric is reported. This change will ensure that each report of 'bans' and 'kicks' reflects new instances since the last report, instead of a cumulative count.

--- src/main/java/de/feelix/sierra/Sierra.java
+++ src/main/java/de/feelix/sierra/Sierra.java
@@ -196,9 +196,16 @@ public final class Sierra extends JavaPlugin implements SierraApi {
     private void initializePacketEvents() {
         Metrics metrics = new Metrics(this, PLUGIN_ID);
 
-        metrics.addCustomChart(new Metrics.SingleLineChart("bans", () -> SierraDataManager.BANS));
-        metrics.addCustomChart(new Metrics.SingleLineChart("kicks", () -> SierraDataManager.KICKS));
-        metrics.addCustomChart(new Metrics.AdvancedPie("checktypes", () -> SierraDataManager.violationCount));
+        metrics.addCustomChart(new Metrics.SingleLineChart("bans", () -> {
+            int bans = SierraDataManager.BANS;
+            SierraDataManager.BANS = 0;
+            return bans;
+        }));
+        metrics.addCustomChart(new Metrics.SingleLineChart("kicks", () -> {
+            int kicks = SierraDataManager.KICKS;
+            SierraDataManager.KICKS = 0;
+            return kicks;
+        }));
 
         PacketEvents.getAPI().getEventManager().registerListener(new PacketListener());
         PacketEvents.getAPI().init();
