652745c3568750135de6c5a7a0ea3990108a2c6b
square <admin@squarecode.de>
2024-07-19 20:17:15 +0200

    Add mitigationOrdinal to MitigationStrategy enum
    The MitigationStrategy enum now includes a new field, mitigationOrdinal. This is an integer that represents the order of the mitigation strategy. Relevant getter methods and JavaDocs have been added for this new field as well.

--- api/src/main/java/de/feelix/sierraapi/violation/MitigationStrategy.java
+++ api/src/main/java/de/feelix/sierraapi/violation/MitigationStrategy.java
@@ -4,18 +4,21 @@ import de.feelix.sierraapi.annotation.NotNull;
 
 public enum MitigationStrategy {
 
-    MITIGATE("mitigated for", "UNAVAILABLE"),
-    KICK("kicked for", "Security Kick"),
-    BAN("punished for", "Security Ban");
+    MITIGATE(1, "mitigated for", "UNAVAILABLE"),
+    KICK(2, "kicked for", "Security Kick"),
+    BAN(3, "punished for", "Security Ban");
 
     @NotNull
     private final String friendlyMessage;
 
+    @NotNull
+    private final int mitigationOrdinal;
 
     @NotNull
     private final String historyMessage;
 
-    MitigationStrategy(String friendlyMessage, String historyMessage) {
+    MitigationStrategy(int mitigationOrdinal, String friendlyMessage, String historyMessage) {
+        this.mitigationOrdinal = mitigationOrdinal;
         this.friendlyMessage = friendlyMessage;
         this.historyMessage = historyMessage;
     }
@@ -29,8 +32,21 @@ public enum MitigationStrategy {
         return friendlyMessage;
     }
 
-
+    /**
+     * Retrieves the history message associated with the mitigation strategy.
+     *
+     * @return The history message associated with the mitigation strategy.
+     */
     public String historyMessage() {
         return historyMessage;
+    }
+
+    /**
+     * Retrieves the mitigation ordinal associated with the mitigation strategy.
+     *
+     * @return The mitigation ordinal.
+     */
+    public int mitigationOrdinal() {
+        return mitigationOrdinal;
     }
 }
